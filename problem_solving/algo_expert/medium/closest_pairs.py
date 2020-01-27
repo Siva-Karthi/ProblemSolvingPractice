31fa343fbbfad1f406b934e86e0e8f7b6ad80f7b
Siva S <sivasubramani.612@gmail.com>
2020-01-27 07:03:01 +0530

    updated closest pair question
    Signed-off-by: Siva S <sivasubramani.612@gmail.com>

--- problem_solving/algo_expert/medium/closest_pairs.py
+++ problem_solving/algo_expert/medium/closest_pairs.py
@@ -1,3 +1,13 @@
+"""
+Write a function that takes in two non-empty arrays of integers. The function should find the pair of numbers
+(one from the first array, one from the second array) whose absolute difference is closest to zero.
+The function should return an array containing these two numbers, with the number from the first array in the
+first position. Assume that there will only be one pair of numbers with the smallest difference.
+
+Sample input: [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]
+Sample output: [28, 26]
+"""
+
 from math import inf
 
 
