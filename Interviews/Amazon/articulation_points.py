96b72cea2f2fc1518ad1ad179a7af0b9d7d5aa43
Siva S <sivasubramani.612@gmail.com>
2020-10-11 10:09:23 +0530

    articulation_points.py added explantion
    Signed-off-by: Siva S <sivasubramani.612@gmail.com>

--- Interviews/Amazon/articulation_points.py
+++ Interviews/Amazon/articulation_points.py
@@ -6,13 +6,28 @@ a graph
   /  |
  3 -  4
 
+ in order 0,1,3,4
+ re order
+ ost order
+
 depth first spanning tree
 
-         0
-      /    |
-     1      2
-  /   |
- 3    4
+   1/10     0
+        /    |
+   2/7  1      2 8/9
+     /   |
+ 3/4 3    4 5/6
+
+
+3                    4
+
+3-4   5-6
+
+1         3
+
+2            7 -
+    3 4 - child
+
 
 breadth first spanning tree
          0
@@ -22,15 +37,43 @@ breadth first spanning tree
  3  -  4
 """
 
-"""
-#         1    
-#      /  |  \  
-#     2 - | - 3 
-#      \  |  /  
-#         4      
-          |  
-#         5             
+# low[child] >= dfn[root]
+#  art
 
+# current 4
+#  5
+# 4 >= 3 =True ;l then 4 is an srticuklation oint or bottleneck
+from pprint import pprint
+
+"""
+#          1    
+#       /  |  \  
+#      2 - | - 3 
+#       \  |  /  
+#          4         low =    1-> default = dfn ; 2 ->  min of the low of all the connections except parent
+           |  
+#          5                 low 2;  min( 1, 1)
+#                            low 4;
+                                                     1 = (3, 1)
+                              if visited -> take min(low(current), dfn[child])
+
+# low {
+   1 : 1
+   4: 1
+   3: 1
+   2: 1
+   
+}
+
+#          1    
+#       /    
+#      2 - - 3 
+#       \     
+#          4      
+           |  
+#          5             
+
+      
 """
 graph3 = {
     "1": ['2', '3', '4'],
@@ -41,13 +84,22 @@ graph3 = {
 }
 
 counter = 0
-dfn = {'1': {'pre': 0, 'post': 0}, '0': {'pre': 0, 'post': 0}, '3': {'pre': 0, 'post': 0}, '2': {'pre': 0, 'post': 0},
+dfn = {'1': {'pre': 0, 'post': 0}, '3': {'pre': 0, 'post': 0}, '2': {'pre': 0, 'post': 0},
        '4': {'pre': 0, 'post': 0}}
 
 visited = []
 low = {}
 articulation = {}
+"""
 
+#    (1/1)          1
+#                 /
+#    (2/2)       2
+#              /  \
+#    (3)      3    4 (4)
+#                   |
+#    (5)            5
+"""
 
 def find_articulation_points(root, parent="-1"):
     print("vertex", root)
@@ -57,15 +109,15 @@ def find_articulation_points(root, parent="-1"):
     counter += 1
     dfn[root] = {'pre': counter}
     low[root] = counter
-    articulation[root] = False
+    # articulation[root] = False
     for child in graph3[root]:
         if child not in visited:
             find_articulation_points(child, root)
             low[root] = min(low[root], low[child])
         elif child != parent:
             low[root] = min(low[root], dfn[child]["pre"])
-        if low[child] >= dfn[root]["pre"]:
-            articulation[root] = True
+        # if low[child] >= dfn[root]["pre"]:
+        #     articulation[root] = True
     # post order number
     counter += 1
     dfn[root]['post'] = counter
@@ -73,7 +125,7 @@ def find_articulation_points(root, parent="-1"):
 
 if __name__ == '__main__':
     find_articulation_points('1')
-    print(dfn)
+    pprint(dfn)
     print(low)
     print(articulation)
 
