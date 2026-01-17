1d40fe583b90ff4c9f518b2a5ac8346c5c77f394
sivasubramani612 <sivasubramani612@gmail.com>
2026-01-17 12:47:35 +0530

    practised count dp
    Signed-off-by: sivasubramani612 <sivasubramani612@gmail.com>

---
+++ problem_solving/dynamic_programming/5_count_dp/climbing_stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+
+if __name__ == '__main__':
+    n = 4
+    s = Solution()
+    print(s.climbStairs(n))
